7b16c5a31870bcff93aca4814a3df12768335ae4
Nguyễn Ngọc Bình <47907431+nguyenngocbinh@users.noreply.github.com>
2020-02-14 17:57:57 +0700

    using mice impute and group df

--- R/predict_plan.R
+++ R/predict_plan.R
@@ -1,9 +1,10 @@
 predict_plan = drake_plan(
-  pred = predict(final_model, df_pred, type = 'response'),
-  write_result = pred %>% 
-    set_names(30000:49999) %>% 
-    as.data.frame() %>% 
-    rownames_to_column() %>% 
-    set_names(c("id", "label")) %>% 
+  m = glm(formula = final_formula, family = binomial(), data = df_gb),
+  pred = predict(m, df_pred, type = 'response'),
+  write_result = pred %>%
+    set_names(30000:49999) %>%
+    as.data.frame() %>%
+    rownames_to_column() %>%
+    set_names(c("id", "label")) %>%
     rio::export("result.csv")
 )
